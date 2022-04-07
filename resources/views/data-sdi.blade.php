79f971fabd2f9227555273e3dacbfc51184d7660
AriSmrd7 <arismrd13@gmail.com>
2022-04-07 21:22:41 +0700

    update

--- resources/views/data-sdi.blade.php
+++ resources/views/data-sdi.blade.php
@@ -24,14 +24,14 @@
                                     @csrf
                                     <input type="hidden" name="id_data" value="{{$id}}">
                                     <div class="row col-lg-12">
-                                        <div class="col-md-4">
+                                        <div class="col-md-3">
                                             <div class="img-bg">
                                                 <img  id="frame" src="{{asset('new-assets/img/notfound.jpg')}}" class="img-fluid" alt="Image">
                                             </div>
                                             <div class="mb-5 mt-3">
                                                 <label>
                                                     <div class="btn btn-info">
-                                                        <i class="fa fa-folder-open"></i>
+                                                        <span class="tf-icons bx bx-folder-open"></span>
                                                         Pilih Gambar
                                                     </div>
                                                     <input class="form-control" type="file" name="foto_map" id="formFile" onchange="preview()" hidden>
@@ -39,21 +39,20 @@
                                             </div>
                                         </div>
 
-                                        <div class="col-md-8">
-                                            <div class="form-group row">
-                                                <label for="ruas" class="col-4 col-form-label">Ruas Jalan</label> 
-                                                <div class="col-8">
-                                                <input id="ruas"  name="ruas" placeholder="Masukkan nama jalan" type="text" class="form-control" required="required">
+                                        <div class="col-md-9">
+                                            <div class="form-group row mb-2">
+                                                <label for="ruas" class="col-md-4 col-form-label">Ruas Jalan</label> 
+                                                <div class="col-md-8">
+                                                <input id="ruas"  name="ruas" placeholder="Masukkan nama jalan" type="text" class="form-control form-control-sm" required="required">
                                                 </div>
                                             </div>
-                                            <div class="form-group row">
-                                                <label for="ruas" class="col-4 col-form-label">STA Awal</label> 
-                                                <div class="col-3">
-                                                <input id="sta_awal" onkeypress="return onlyNumber(event, false)"   maxlength="4" name="sta_awal"  placeholder="0" type="text" class="form-control" required="required">
-                                                </div>
-                                                <span style="font-size: x-large;">+</span>
-                                                <div class="col-3">
-                                                <select name="sta_awal2" class="form-control">
+                                            <div class="form-group row  mb-2">
+                                                <label for="ruas" class="col-md-4 col-form-label">STA Awal</label> 
+                                                <div class="col-md-5">
+                                                <div class="input-group input-group-sm">
+                                                    <input id="sta_awal" onkeypress="return onlyNumber(event, false)"   maxlength="4" name="sta_awal"  placeholder="0" type="text" class="form-control" required="required">
+                                                    <span class="input-group-text">+</span>
+                                                    <select name="sta_awal2" class="form-select">
                                                         <option value="000">000</option>
                                                         <option value="100">100</option>
                                                         <option value="200">200</option>
@@ -64,17 +63,17 @@
                                                         <option value="700">700</option>
                                                         <option value="800">800</option>
                                                         <option value="900">900</option>
-                                                    </select>                                                
+                                                    </select>       
+                                                </div>
                                                 </div>
                                             </div>
-                                            <div class="form-group row">
-                                                <label for="ruas" class="col-4 col-form-label">STA Akhir</label> 
-                                                <div class="col-3">
-                                                <input id="sta_akhir" onkeypress="return onlyNumber(event, false)"   maxlength="4" name="sta_akhir" placeholder="0" type="text" class="form-control" required="required">
-                                                </div>
-                                                <span style="font-size: x-large;">+</span>
-                                                <div class="col-3">
-                                                    <select name="sta_akhir2" class="form-control">
+                                            <div class="form-group row  mb-2">
+                                                <label for="ruas" class="col-md-4 col-form-label">STA Akhir</label> 
+                                                <div class="col-md-5">
+                                                <div class="input-group input-group-sm">
+                                                    <input id="sta_awal" onkeypress="return onlyNumber(event, false)"   maxlength="4" name="sta_akhir"  placeholder="0" type="text" class="form-control" required="required">
+                                                    <span class="input-group-text">+</span>
+                                                    <select name="sta_akhir2" class="form-select">
                                                         <option value="000">000</option>
                                                         <option value="100">100</option>
                                                         <option value="200">200</option>
@@ -85,24 +84,23 @@
                                                         <option value="700">700</option>
                                                         <option value="800">800</option>
                                                         <option value="900">900</option>
-                                                    </select>
+                                                    </select>       
+                                                </div>
                                                 </div>
                                             </div>
-                                            <div class="form-group row">
-                                                <label for="lebar" class="col-4 col-form-label">Lebar</label> 
-                                                <div class="col-6">
-                                                <div class="input-group">
-                                                    <input id="lebar" name="lebar" min="0" placeholder="0" type="number" class="form-control" required="required"> 
-                                                    <div class="input-group-append">
-                                                    <div class="input-group-text">Meter</div>
+                                            <div class="form-group row mb-2">
+                                                <label for="lebar" class="col-md-4 col-form-label">Lebar</label> 
+                                                <div class="col-md-3">
+                                                    <div class="input-group input-group-sm">
+                                                        <input id="lebar"  name="lebar" min="0" placeholder="0" type="text" class="form-control" maxlength="6" onkeypress="return isNumberKey(event,this)" required="required"> 
+                                                        <span class="input-group-text">Meter</span>
                                                     </div>
                                                 </div>
-                                                </div>
                                             </div>
-                                            <div class="form-group row">
-                                                <label for="lajur" class="col-4 col-form-label">Jumlah Lajur</label> 
-                                                <div class="col-8">
-                                                <select id="lajur" name="lajur" class="form-control" required="required">
+                                            <div class="form-group row mb-2">
+                                                <label for="lajur" class="col-md-4 col-form-label">Jumlah Lajur</label> 
+                                                <div class="col-md-4">
+                                                <select id="lajur" name="lajur" class="form-select form-select-sm" required="required">
                                                     <option disabled selected><i class="text-muted">Pilih jumlah lajur...</i></option>
                                                     <option value="1">1 Lajur</option>
                                                     <option value="2">2 Lajur</option>
@@ -111,10 +109,10 @@
                                                 </select>
                                                 </div>
                                             </div>
-                                            <div class="form-group row">
-                                                <label for="jalur" class="col-4 col-form-label">Jumlah Jalur</label> 
-                                                <div class="col-8">
-                                                <select id="jalur" name="jalur" class="form-control" required="required">
+                                            <div class="form-group row mb-2">
+                                                <label for="jalur" class="col-md-4 col-form-label">Jumlah Jalur</label> 
+                                                <div class="col-md-4">
+                                                <select id="jalur" name="jalur" class="form-select form-select-sm" required="required">
                                                     <option disabled selected><i class="text-muted">Pilih jumlah jalur...</i></option>
                                                     <option value="1">1 Jalur</option>
                                                     <option value="2">2 Jalur</option>
@@ -122,10 +120,10 @@
                                                 </select>
                                                 </div>
                                             </div>
-                                            <div class="form-group row">
-                                                <label for="arah" class="col-4 col-form-label">Jumlah Arah</label> 
-                                                <div class="col-8">
-                                                <select id="arah" name="arah" class="form-control" required="required">
+                                            <div class="form-group row mb-2">
+                                                <label for="arah" class="col-md-4 col-form-label">Jumlah Arah</label> 
+                                                <div class="col-md-4">
+                                                <select id="arah" name="arah" class="form-select form-select-sm" required="required">
                                                     <option disabled selected><i class="text-muted">Pilih jumlah arah...</i></option>
                                                     <option value="1">1 Arah</option>
                                                     <option value="2">2 Arah</option>
@@ -133,25 +131,20 @@
                                                 </div>
                                             </div>
                                             <div class="form-group row">
-                                                <label for="tipe" class="col-4 col-form-label">Tipe Perkerasan</label> 
-                                                <div class="col-8">
-                                                <select id="tipe" name="tipe" required="required" class="form-control">
+                                                <label for="tipe" class="col-md-4 col-form-label">Tipe Perkerasan</label> 
+                                                <div class="col-md-4">
+                                                <select id="tipe" name="tipe" required="required" class="form-select form-select-sm">
                                                     <option disabled selected><i class="text-muted">Pilih tipe...</i></option>
                                                     <option value="Lentur">Lentur</option>
                                                     <option value="Kaku">Kaku</option>
                                                 </select>
                                                 </div>
                                             </div> 
-                                            <div class="form-group row mt-4">
-                                                <div class="col-4">
-                                                <button name="reset" type="reset" class="btn btn-block btn-secondary">
-                                                <i class="fa fa-refresh"></i> Reset
-                                                </button>
-                                                </div>
-                                                <div class="col-8">
-                                                <button name="submit" type="submit" class="btn btn-block btn-primary">
-                                                    <i class="fa fa-save"></i> Simpan
-                                                </button>
+                                            <div class="form-group row mt-3">
+                                                <div class="d-grid gap-2 d-md-block">
+                                                    <button name="submit" type="submit" class="btn btn-block btn-primary">
+                                                    <span class="tf-icons bx bx-save"></span>  Simpan
+                                                    </button>
                                                 </div>
                                             </div>
                                         </div>
@@ -198,4 +191,27 @@
             }
 
         </script>
+        
+<script>
+    function isNumberKey(evt, element) {
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57) && !(charCode == 46 || charCode == 8))
+        return false;
+    else {
+        var len = $(element).val().length;
+        var index = $(element).val().indexOf('.');
+        if (index > 0 && charCode == 46) {
+        return false;
+        }
+        if (index > 0) {
+        var CharAfterdot = (len + 1) - index;
+        if (CharAfterdot > 3) {
+            return false;
+        }
+        }
+
+    }
+    return true;
+    }
+</script>
 @endpush
