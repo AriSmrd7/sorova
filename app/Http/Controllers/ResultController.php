b6c105df387894be54cb071019701d577449aaef
AriSmrd7 <arismrd13@gmail.com>
2022-04-11 03:27:06 +0700

    update

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\DetailSta;
 use App\Models\DataSdi;
 use App\Models\Stationing;
+use Illuminate\Support\Facades\DB;
 
 class ResultController extends Controller
 {
@@ -28,10 +29,21 @@ class ResultController extends Controller
     {
         $dataPrimer = DataSdi::where('id',$id)->first();
 
-        $dataSta = Stationing::where('id_data',$id)->paginate(20);;
+        $dataSta = Stationing::where('id_data',$id)->paginate(25);;
         $maxBR = DetailSta::where('id_data',$id)->max('bekas_roda');
         $maxLR = DetailSta::where('id_data',$id)->max('lebar_retak');
-        return view('result',compact('dataPrimer','dataSta','maxBR','maxLR'))
-        ->with('i', (request()->input('page', 1) - 1) * 20);
+
+        $resultData = DB::table('tb_stationing')
+                            ->leftJoin('tb_result', function($join)
+                                {
+                                    $join->on('tb_stationing.id_data', '=', 'tb_result.id_data');
+                                    $join->on('tb_stationing.nama_sta', '=', 'tb_result.id_sta');
+                                })
+                            ->groupBy('tb_stationing.id_sta')
+                            ->having('tb_stationing.id_data','=',$id)
+                          ->get();
+
+        return view('result',compact('dataPrimer','dataSta','maxBR','maxLR','resultData'))
+        ->with('i', (request()->input('page', 1) - 1) * 25);
     }
 }
--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -20,6 +20,11 @@
                         </button>
                       </li>
                       <li class="nav-item">
+                        <button type="button" class="nav-link" role="tab" data-bs-toggle="tab" data-bs-target="#navs-pills-top-hasil" aria-controls="navs-pills-top-hasil" aria-selected="false">
+                          Nilai SDI
+                        </button>
+                      </li>
+                      <li class="nav-item">
                         <button type="button" class="nav-link" role="tab" data-bs-toggle="tab"  data-bs-target="#navs-pills-top-chart"  aria-controls="navs-pills-top-chart" ria-selected="false">
                           Chart
                         </button>
@@ -40,47 +45,48 @@
                           </div>
 
                           <div class="col-md-9">
-                            <small class="text-light fw-semibold">Data Primer </small>
+                          <small class="text-light fw-semibold me-5">Data Primer </small>
                             <div class="demo-inline-spacing mt-2">
                               <ul class="list-group">
                                 <li class="list-group-item d-flex align-items-center">
                                   <i class="bx bx-map-pin me-2"></i>
-                                  <div class="col-3">Ruas Jalan</div>
+                                  <div class="col-3 me-4">Ruas Jalan</div>
                                   <div class="text-primary">{{$dataPrimer->ruas_jalan}}</div> 
                                 </li>
                                 <li class="list-group-item d-flex align-items-center">
                                   <i class="bx bx-station me-2"></i>
-                                  <div class="col-3">Stationing</div>
+                                  <div class="col-3 me-4">Stationing</div>
                                   <div class="text-primary">{{$dataPrimer->sta_awal}} <i class="bx bx-chevron-right"></i> {{$dataPrimer->sta_akhir}}</div>                               
                                 </li>
                                 <li class="list-group-item d-flex align-items-center">
                                   <i class="bx bx-tachometer me-2"></i>
-                                  <div class="col-3">Lebar Jalan</div>
+                                  <div class="col-3 me-4">Lebar Jalan</div>
                                   <div class="text-primary">{{$dataPrimer->lebar}} Meter</div> 
                                 </li>
                                 <li class="list-group-item d-flex align-items-center">
                                   <i class="bx bx-trip me-2"></i>
-                                  <div class="col-3">Jumlah Lajur</div>
+                                  <div class="col-3 me-4">Jumlah Lajur</div>
                                   <div class="text-primary">{{$dataPrimer->jumlah_lajur}} Lajur</div>                               
                                 </li>
                                 <li class="list-group-item d-flex align-items-center">
                                   <i class="bx bx-sort-alt-2 me-2"></i>
-                                  <div class="col-3">Jumlah Jalur</div>
+                                  <div class="col-3 me-4">Jumlah Jalur</div>
                                   <div class="text-primary">{{$dataPrimer->jumlah_jalur}} Jalur</div>                               
                                 </li>
                                 <li class="list-group-item d-flex align-items-center">
                                   <i class="bx bx-directions me-2"></i>
-                                  <div class="col-3">Jumlah Arah</div>
+                                  <div class="col-3 me-4">Jumlah Arah</div>
                                   <div class="text-primary">{{$dataPrimer->jumlah_arah}} Arah</div>                               
                                 </li>
                                 <li class="list-group-item d-flex align-items-center">
                                   <i class="bx bx-hive me-2"></i>
-                                  <div class="col-3">Tipe Perkerasan</div>
+                                  <div class="col-3 me-4">Tipe Perkerasan</div>
                                   <div class="text-primary">{{$dataPrimer->tipe_perkerasan}}</div>                               
                                 </li>
                               </ul>
                             </div>
                           </div>
+                          <small class="text-light fw-semibold mt-2">Tanggal Input : {{date('l, j F Y , h:i A', strtotime($dataPrimer->created_at))}} </small>
                         </div> 
                       </div>
 
@@ -149,6 +155,54 @@
                           </div>
                         </div>
 
+
+                        <div class="tab-pane fade" id="navs-pills-top-hasil" role="tabpanel">
+                          <div class="col-sm-12">
+                            <small class="text-light fw-semibold">Nilai SDI</small>
+                              <div class="demo-inline-spacing mt-2">
+                                <div class="col-12">
+                                  <div class="table-responsive">
+                                    <table class="table table-bordered">
+                                      <thead class="text-center">
+                                        <tr>
+                                          <th rowspan="2" class="align-middle" width="1%">No.</th>
+                                          <th rowspan="2" class="align-middle" width="25%">STA</th>
+                                          <th colspan="4">Nilai SDI</th>
+                                          <th rowspan="4" class="align-middle" width="15%">Kondisi Jalan</th>
+                                        </tr>
+                                        <tr>
+                                          <th>SDI 1</th>
+                                          <th>SDI 2</th>
+                                          <th>SDI 3</th>
+                                          <th>SDI 4</th>
+                                        </tr>
+                                      </thead>
+                                      <tbody class="text-center">
+                                        @foreach($resultData as $key => $rowRes)
+                                          <tr>
+                                            <td>{{$loop->iteration}}
+                                            <td>{{substr_replace($rowRes->id_sta, '+', 1, 0)}} ‒ @if(!$loop->last) {{substr_replace($resultData[$key+1]->id_sta, '+', 1, 0)}} @else {{substr_replace($resultData[$key]->id_sta+100, '+', 1, 0)}} @endif</td>
+                                            <td>{{$rowRes->sdi_1}}</td>
+                                            <td>{{$rowRes->sdi_2}}</td>
+                                            <td>{{$rowRes->sdi_3}}</td>
+                                            <td>{{$rowRes->sdi_4}}</td>
+                                            @if($rowRes->kondisi_jalan == 'RUSAK BERAT') @php $bgBadge = 'bg-danger'; @endphp
+                                            @elseif($rowRes->kondisi_jalan == 'RUSAK RINGAN')  @php $bgBadge = 'bg-warning'; @endphp
+                                            @elseif($rowRes->kondisi_jalan == 'SEDANG') @php $bgBadge = 'bg-info'; @endphp
+                                            @elseif($rowRes->kondisi_jalan == 'BAIK')  @php  $bgBadge = 'bg-success'; @endphp
+                                            @else @php $bgBadge = ''; @endphp
+                                            @endif
+
+                                            <td><span class="badge {{$bgBadge}}">{{$rowRes->kondisi_jalan}}</span></td>
+                                          </tr>
+                                        @endforeach
+                                      </tbody>
+                                    </table>
+                                  </div>
+                                </div>
+                              </div>
+                          </div>
+                        </div>
 
                         <div class="tab-pane fade" id="navs-pills-top-chart" role="tabpanel">
                           <p>
